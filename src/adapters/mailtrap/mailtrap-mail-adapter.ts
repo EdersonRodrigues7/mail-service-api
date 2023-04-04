689d976b05a1769e2a6aec9eaeedea3ebdb6cab9
Ederson Rodrigues <ederson.rodrigues777@gmail.com>
2023-04-04 00:03:13 -0300

    fix security

--- src/adapters/mailtrap/mailtrap-mail-adapter.ts
+++ src/adapters/mailtrap/mailtrap-mail-adapter.ts
@@ -2,10 +2,10 @@ import { MailtrapClient } from "mailtrap";
 import { MailAdapter, SendMailData } from '../mail-adapter';
 
 // Production
-const TOKEN = "********6bd5";
+const TOKEN = process.env.MAILTRAP_PASSWORD || "";
 const SENDER_EMAIL = "ederson@d-dev.dev";
 
-const client = new MailtrapClient({ endpoint: process.env.MAILTRAP_HOST, token: TOKEN });
+const client = new MailtrapClient({ endpoint: "https://send.api.mailtrap.io/", token: TOKEN });
 
 const sender = { name: "D-Dev Mail Service", email: SENDER_EMAIL };
 
@@ -13,11 +13,14 @@ export class MailtrapMailAdapter implements MailAdapter {
     async sendMail ({ destination, subject, body }: SendMailData){
         client.send({
             from: sender,
-            to: [{name: destination.name, email: destination.email}],
+            to: [{email: destination.email, name: destination.name}],
             subject: subject,
             html: body
         })
         .then(res => console.log(res))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err); 
+            throw new Error("Error sending mail!");
+        });
     };
 }
